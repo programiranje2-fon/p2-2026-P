d15af23a0c8e1cb3c374fe2fecbd7a22c805fa95
devedzic <devedzic@gmail.com>
2026-03-17 22:49:33 +0100

    Project structure

---
+++ blankscripts/stringsblank.py
@@ -0,0 +1,27 @@
+"""Demonstrates working with strings in Python.
+"""
+
+
+#%%
+def demonstrate_fancy_formatting_with_f_strings():
+    """Using f-strings in formatting.
+    - format strings like f'Some text {some var}, more text {another var}...', etc.
+    """
+
+
+#%%
+# Test demonstrate_fancy_formatting_with_f_strings()
+demonstrate_fancy_formatting_with_f_strings()
+
+#%%
+
+
+def demonstrate_string_operations():
+    """Using different string operations.
+    - endswith(), split(), center(), in (aka contains()), == (aka equals()), len(), ..., strip() (lstrip(), rstrip())
+    """
+
+
+#%%
+# Test demonstrate_string_operations()
+demonstrate_string_operations()
